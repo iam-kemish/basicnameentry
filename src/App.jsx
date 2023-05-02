4c6717925902f036929bd8a1627f7cf641be5095
kemish bajgai <kemishbajgai4@gmail.com>
2023-05-02 22:00:04 +0545

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./Component/Home"
+
 function App() {
  
   return (
     <div className="container my-3">
+      <h1 style={{textAlign: "center"}}>A basic form list.</h1>
    <Home/>
   </div>
  
